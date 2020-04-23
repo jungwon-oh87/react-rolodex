270e8dd897e771522674f557b780c4b236318c58
Jungwon Oh <jungwon.oh87@gmail.com>
2020-04-22 21:37:30 -0700

    Updates

---
+++ precache-manifest.674df4ade0386a60bf30296aab09c222.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5ad6f225569347be7d31a4c8656ad2a",
+    "url": "/react-rolodex/index.html"
+  },
+  {
+    "revision": "e15001b318c189351e8f",
+    "url": "/react-rolodex/static/css/main.9626a481.chunk.css"
+  },
+  {
+    "revision": "739cc9dfd803a4f9b832",
+    "url": "/react-rolodex/static/js/2.f2f65767.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-rolodex/static/js/2.f2f65767.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e15001b318c189351e8f",
+    "url": "/react-rolodex/static/js/main.03ad2b39.chunk.js"
+  },
+  {
+    "revision": "80c99a0accea5b75f9ee",
+    "url": "/react-rolodex/static/js/runtime-main.9e9db64f.js"
+  }
+]);
