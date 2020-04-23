6680ddcf9629541c5661323badfce9b6dcdbe648
Jungwon Oh <jungwon.oh87@gmail.com>
2020-04-22 21:25:22 -0700

    styling added

--- src/App.js
+++ src/App.js
@@ -42,6 +42,7 @@ class App extends Component {
 
     return (
       <div className="App">
+        <h2>Cat Rolodex</h2>
         <InputBox
           placeholder="Search your kitty"
           handleChange={this.handleInputChange}
